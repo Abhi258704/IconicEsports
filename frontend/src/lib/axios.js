b07d593e3de721e6820a4b52169901cde87e8ccf
Baby-Coder <abhishekdinesh357@gmail.com>
2026-05-20 18:01:32 +0530

    backend base url

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-   baseURL: "http://localhost:8000/api/v1",
+   baseURL: process.env.BACKEND_BASE_URL,
 });
 
 API.interceptors.request.use((config) => {
